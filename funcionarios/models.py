1fa24a580868a396a3683c0716fb29de232ab4c2
SamuelHenrique007 <sh0161663@gmail.com>
2025-07-13 22:24:17 -0300

    adiciona relacionamento entre Funcionario e User, implementa criação de funcionário com validaçõe

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,8 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 class Funcionario(models.Model):
-    
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     permissao = models.CharField(max_length=20, choices=[('admin', 'Administrador'), ('vendedor', 'Vendedor')])
 
--- funcionarios/views.py
+++ funcionarios/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import permission_required
+from functools import wraps
 
 @login_required
 def home(request):
@@ -16,16 +17,37 @@ def listar_funcionarios(request):
     funcionarios = Funcionario.objects.all()
     return render(request, 'funcionarios/funcionarios_lista.html', {'funcionarios': funcionarios})
 
+
 @login_required
 def criar_funcionario(request):
     if request.method == 'POST':
-        form = FuncionarioForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/funcionarios/')
-    else:
-        form = FuncionarioForm()
-    return render(request, 'funcionarios/funcionarios_form.html', {'form': form})
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        permissao = request.POST.get('permissao')
+
+        if not all([nome, email, senha, permissao]):
+            messages.error(request, "Todos os campos são obrigatórios.")
+            return redirect('criar_funcionario')
+
+        if User.objects.filter(username=nome).exists():
+            messages.error(request, "Já existe um usuário com esse e-mail.")
+            return redirect('criar_funcionario')
+
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+
+        # AQUI dá erro se o modelo não estiver certo
+        Funcionario.objects.create(
+            usuario=user,
+            nome=nome,
+            permissao=permissao
+        )
+
+        messages.success(request, "Funcionário criado com sucesso!")
+        return redirect('listar_funcionarios')
+
+    return render(request, 'funcionarios/funcionarios_form.html', {})
+
 
 @login_required
 def editar_funcionario(request, funcionario_id):
@@ -49,4 +71,7 @@ def deletar_funcionario(request, funcionario_id):
 def detalhes_funcionarios(request, funcionario_id):
     funcionario = get_object_or_404(Funcionario, id=funcionario_id)
     return render(request, 'funcionarios/funcionarios_detalhe.html', {'funcionario': funcionario})
+
+
+
     
